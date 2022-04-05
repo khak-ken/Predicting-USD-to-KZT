ebe9f5735636a696bd904df09f0fe83f936ae172
khak-ken <103071571+khak-ken@users.noreply.github.com>
2022-04-06 00:51:40 +0500

    Add files via upload

---
+++ USD.R
@@ -0,0 +1,20 @@
+setwd("C:/Users/nhlr/OneDrive - Chevron/Documents/")
+#install.packages("readxl") # CRAN version
+library("xlsx")
+
+library(readxl)
+train=read_excel("Official_Foreign_Exchange_Rates_NBRK_on_29_12_2018.xls")
+test=read_excel("2019 data USD.xlsx")
+
+
+
+library(randomForest) 
+# Perform training: 
+fit = randomForest(USD ~ ., data=train, ntree=100,importance=TRUE) 
+
+
+Prediction <- predict(fit, test)
+
+submit<-data.frame(Date = test$Date, USD = Prediction)
+write.csv(submit, file = "USD Prediction.csv", row.names = FALSE)
+
